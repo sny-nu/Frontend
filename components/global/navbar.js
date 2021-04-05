8b9df3a0c68c333379b0825d38cd3e88f95aaac8
Thomas Lamars <thomas@lamars.nl>
2021-04-05 12:08:47 +0200

    Created cookie banner and changed urls in footer

--- components/global/navbar.js
+++ components/global/navbar.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Hamburger from './svg/hamburger';
 import Close from './svg/close';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function Navbar() {
     const [ isOpen, setOpen ] = useState(false);
@@ -14,15 +14,15 @@ export default function Navbar() {
                 </div>
                 <div className={(isOpen == true ? "z-40 fixed top-0 left-0 h-full w-full bg-gray-900 flex flex-col justify-center" : "hidden") + " md:w-3/4  md:flex-row md:flex"}>
                     <div className="p-0 flex md:flex-row flex-col items-center md:w-2/3 text-white justify-center w-full">
-                    {/* HomePage */}
-                    <Link href="/">
-                        <a onClick={() => setOpen(isOpen => false)}
-                            className="inline-block py-2 mx-2 md:font-medium font-semibold md:px-0 lg:mx-3 md:text-center 
-                                focus:text-green-500 hover:text-green-500 transition duration-300 ease-in-out">
-                            Home
-                        </a>
-                    </Link>
-                </div>
+                        {/* HomePage */}
+                        <Link href="/">
+                            <a onClick={() => setOpen(isOpen => false)}
+                                className="inline-block py-2 mx-2 md:font-medium font-semibold md:px-0 lg:mx-3 md:text-center 
+                                    focus:text-green-500 hover:text-green-500 transition duration-300 ease-in-out">
+                                Home
+                            </a>
+                        </Link>
+                    </div>
                 </div>
                 <div className="md:hidden z-50">
                     <button type="button" onClick={() => setOpen(isOpen => !isOpen)} className="block text-gray-500 pr-3 hover:text-white focus:text-white focus:outline-none">
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,32 @@
 import { ToastContainer } from 'react-toastify'
+import cookie from 'react-cookies'
 import 'react-toastify/dist/ReactToastify.css';
+import Cookies from '../components/global/cookies';
 import '../styles/globals.css'
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+    const [snConsent, setSnConsent] = useState(undefined);
+
+    useEffect(() => {
+        snConsent == undefined && setSnConsent(cookie.load("sn-cookie-consent"));
+    }, [snConsent]);
+
+    function setCookie(e) {
+        const cookieValue = e.target.value;
+
+        const expires = new Date()
+            expires.setDate(Date.now() + 1000 * 60 * 60 * 24 * 14)
+
+        setSnConsent(cookieValue);
+        cookie.save('sn-cookie-consent', cookieValue, {
+            expires
+        })
+    }
+
     return (
         <>
+            { snConsent === undefined ? <Cookies onClick={setCookie} /> : null }
             <Component {...pageProps} />
             <ToastContainer className="rounded-full text-sm" style={{ width: "400px" }} />
         </>
