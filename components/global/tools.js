bda05d47646d3f2372b6fb10047c3fc7db6d1191
Thomas Lamars <thomas@lamars.nl>
2021-04-07 20:58:45 +0200

    Added Sentry integration

--- components/global/tools.js
+++ components/global/tools.js
@@ -1,7 +1,20 @@
 import useAckee from "../../hooks/useAckee";
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 export default function Tools() {
     useAckee();
+    
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        integrations: [new Integrations.BrowserTracing()],
+    
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    });
 
     return <></>;
 }
