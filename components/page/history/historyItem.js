ae8f7e559be3595fe4b07f0bd973126f672e75d3
Thomas Lamars <thomas@lamars.nl>
2021-04-08 21:55:13 +0200

    Removed two typos

--- components/page/history/historyItem.js
+++ components/page/history/historyItem.js
@@ -26,7 +26,7 @@ export default function HistoryItem({ url }) {
                 <div className="bg-gray-800 rounded-full my-4 md:my-0 text-white flex flex-row">
                     <div className="text-left truncate w-11/12 py-4 px-6">
                         <p className="text-base font-bold">{ url.shortUrl }</p>
-                        <div className="text-xs truncate">{ url.originalUrl }/123132183198371239812731298373219837129831273891273129387123891273192837129382137192837129381798 312x17n9812nx129x37n1293x71n239712x371237</div>
+                        <div className="text-xs truncate">{ url.originalUrl }</div>
                     </div>
                     <button className="rounded-full w-16 h-14 m-2 p-auto bg-green-500 focus:outline-none hover:bg-green-400 transition ease-in-out duration-300 hover:text-gray-700" onClick={clickCopy}>
                         <Copy position="center" />
