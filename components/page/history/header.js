d74e2b6e8390dc2a524e381291770d0e085f4584
Thomas Lamars <thomas@lamars.nl>
2021-04-08 21:44:03 +0200

    Message that cookies are not accepted

--- components/page/history/header.js
+++ components/page/history/header.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import HistoryItem from "./historyItem";
+import cookie from 'react-cookies';
 
 export default function HistoryHeader() {
     const [history, setHistory] = useState([]);
+
+    const cookieValue = cookie.load('sn-cookie-consent') === "true";
 
     useEffect(() => {
         const jsonHistory = localStorage.getItem("sn-history");
@@ -17,16 +20,26 @@ export default function HistoryHeader() {
                 Your sny<span className="text-green-500">.</span>nu history
             </h1>
             <p className="max-w-lg mx-auto mt-3 text-sm text-center text-gray-400 
-                    md:mt-6 sm:text-base md:max-w-xl md:text-lg xl:text-lg"
+                md:mt-6 sm:text-base md:max-w-xl md:text-lg xl:text-lg"
             >
                 Did you ever make a sny url and lost it? <br />
                 Well you found the correct page! Below are all your shortened urls that you made from this webbrowser.
             </p>
-            <div className="md:grid gap-4 m-4 sm:grid-cols-1 md:grid-cols-2 lg:w-12/12 xl:w-8/12 w-full mx-auto mt-6">
-                {history.map((url) => (
-                    <HistoryItem url={url} key={url.hash} />
-                ))}
-            </div>
+            { cookieValue ? (
+                <div className="md:grid gap-4 m-4 sm:grid-cols-1 md:grid-cols-2 lg:w-12/12 xl:w-8/12 w-full mx-auto mt-6">
+                    {history.map((url) => (
+                        <HistoryItem url={url} key={url.hash} />
+                    ))}
+                </div>
+            ) : (
+                <p className="max-w-lg mx-auto mt-3 text-sm text-center text-gray-400 
+                    md:mt-6 sm:text-base md:max-w-xl md:text-lg xl:text-lg"
+                >
+                    Unfortunatly you have didn't accept or rejected cookies. <br />
+                    This is why you can't see a history of your created sny.nu urls.
+                </p>
+                )
+            }
         </div>
     )
 }
