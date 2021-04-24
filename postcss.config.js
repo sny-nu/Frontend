d25e6ec75529db61d177e8464f99b8c0f6846fee
Thomas Lamars <thomas@lamars.nl>
2021-04-24 19:30:13 +0200

    Change in postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                loader: 'postcss-loader',
-                options: {
-                    postcssOptions: {
-                        syntax: 'postcss',
-                        plugins: ['postcss-import', 'tailwindcss', 'postcss-nested', 'autoprefixer'],
-                    },
-                },
-            },
-        ],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
     },
-};
+}
